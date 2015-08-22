0209907e0711161612f9f1ec34b7a65a287ea310
andreykurenkov <andrey.sendme@gmail.com>
2015-08-22 04:30:51 -0700

    Starting on Android app, config and Beacon finding code

--- Android/TransitTimes/mobile/src/main/java/thingswithworth/org/transittimes/MainActivity.java
+++ Android/TransitTimes/mobile/src/main/java/thingswithworth/org/transittimes/MainActivity.java
@@ -1,17 +1,30 @@
 package thingswithworth.org.transittimes;
 
+import android.os.RemoteException;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.utils.UrlBeaconUrlCompressor;
 
-public class MainActivity extends ActionBarActivity {
+import java.util.Collection;
 
+
+public class MainActivity extends ActionBarActivity implements BeaconConsumer, RangeNotifier {
+    private static String TAG = "MainActivity";
+    private BeaconManager mBeaconManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,5 +73,43 @@ public class MainActivity extends ActionBarActivity {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
+        // Detect the URL frame:
+        mBeaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("s:0-1=feaa,m:2-2=10,p:3-3:-41,i:4-20"));
+        mBeaconManager.bind(this);
+    }
+
+    public void onBeaconServiceConnect() {
+        Region region = new Region("all-beacons-region", null, null, null);
+        try {
+            mBeaconManager.startRangingBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        mBeaconManager.setRangeNotifier(this);
+    }
+
+    @Override
+    public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+        for (Beacon beacon: beacons) {
+            if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x10) {
+                // This is a Eddystone-URL frame
+                String url = UrlBeaconUrlCompressor.uncompress(beacon.getId1().toByteArray());
+                Log.d(TAG, "I see a beacon transmitting a url: " + url +
+                        " approximately " + beacon.getDistance() + " meters away.");
+            }
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mBeaconManager.unbind(this);
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hack-my-ride
 Work on Hack My Ride
+
+Needed installation beyond virtualenv:
+https://docs.djangoproject.com/en/1.8/ref/contrib/gis/install/geolibs/
+https://docs.djangoproject.com/en/1.8/ref/contrib/gis/install/postgis/
