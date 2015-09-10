aea8e19f15e320f08aee354187ba3d8d0a28901b
Ubuntu <azureuser@HackMyRide.HackMyRide.b8.internal.cloudapp.net>
2015-09-10 06:26:15 +0000

    Additional real time endpoints+fixed to work as JSON, work on website UI

--- Django/realtimerestapi/urls.py
+++ Django/realtimerestapi/urls.py
@@ -6,8 +6,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'stop/realtime/(?P<stop_id>[^/]+)/?$',
+    url(r'stop/realtimemonitor/(?P<stop_id>[^/]+)/?$',
         StopRealTimeAPIView.as_view(), name='stop_realtime_detail'),
     url(r'trips/realtime/(?P<agency_name>[^/]+)/?$',
-        TripRealTimeUpdatesAPIView.as_view(), name='trips_realtime_update')
+        TripsRealTimeUpdatesAPIView.as_view(), name='trips_realtime_update'),
+    url(r'trip/realtime/(?P<trip_id>[^/]+)/?$',
+        TripRealTimeUpdatesAPIView.as_view(), name='trip_realtime_update'),
+    url(r'stop_time/realtime/(?P<stop_time_id>[^/]+)/?$',
+        StopRealTimeUpdatesAPIView.as_view(), name='stop_time_realtime_update')
 )
