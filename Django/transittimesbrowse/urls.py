4eee5ea310b6466500668409a4f1ee7ebd408006
Andrey Kurenkov <andrey.sendme@gmail.com>
2015-09-16 23:20:45 +0000

    Minor changes to GUI

--- Django/transittimesbrowse/urls.py
+++ Django/transittimesbrowse/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     url(r'about/$', 'transittimesbrowse.views.about', name='about'),
     url(r'(?P<agency_id>.+)/routes/$', 'transittimesbrowse.views.routes', name='routes'),
     url(r'(?P<agency_id>.+)/routes/$', 'transittimesbrowse.views.routes', name='routes'),
-    url(r'(?P<agency_id>.+)/routes/(?P<route_id>\d+)/stops/$', 'transittimesbrowse.views.stops', name='stops'),
-    url(r'(?P<agency_id>.+)/routes/(?P<route_id>\d+)/stops/(?P<stop_id>\d+)/$', 'transittimesbrowse.views.stop', name='stop'),
-    url(r'(?P<agency_id>.+)/routes/(?P<route_id>\d+)/stops/(?P<stop_id>\d+)/trip/(?P<trip_id>\d+)$', 'transittimesbrowse.views.trip', name='trip')
+    url(r'(?P<agency_id>.+)/routes/(?P<route_id>.+)/stops/$', 'transittimesbrowse.views.stops', name='stops'),
+    url(r'(?P<agency_id>.+)/routes/(?P<route_id>.+)/stops/(?P<stop_id>\d+)/$', 'transittimesbrowse.views.stop', name='stop'),
+    url(r'(?P<agency_id>.+)/routes/(?P<route_id>.+)/stops/(?P<stop_id>\d+)/trip/(?P<trip_id>\d+)$', 'transittimesbrowse.views.trip', name='trip')
 )
